8c92e79b5d4f45a8608381da3c826fb81603597c
Mixa-123098 <60194946+Mixa-123098@users.noreply.github.com>
2024-05-23 18:10:19 +0300

    Update RegistrationForm.jsx

--- src/reg/auth/RegistrationForm.jsx
+++ src/reg/auth/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import authStore from "../../store/authStore";
